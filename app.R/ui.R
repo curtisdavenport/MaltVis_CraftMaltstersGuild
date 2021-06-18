7e97fd10b6d2b30acc0e0e5d009f27fa536bb00e
andrewcaffreyphd <72470158+andrewcaffreyphd@users.noreply.github.com>
2021-06-17 18:15:07 -0700

    Caffrey UI Update

--- app.R/ui.R
+++ app.R/ui.R
@@ -32,11 +32,11 @@ ui <- navbarPage(title=div(img(src="cropped-CMG_Web_logo_small.png",
                           strong("UPLOAD"), "Use this tab to upload your data as a .csv file into MaltVis. Or, use the sample data set provided. Don't worry, your data will not be collected,stored, or shared.",
                           br(),
                           br(),
-                          strong("BATCH SUMMARY/STATS"),"displays a chart and table comparing the malt quality data of a selected batch to previous batches of the same style. Enter a Batch ID, choose the number of previous batches to compare it to, and voila!
+                          strong("BATCHVIS"),"displays a chart and table comparing the malt quality data of a selected batch to previous batches of the same style. Enter a Batch ID, choose the number of previous batches to compare it to, and voila!
 ",
                           br(),
                           br(),
-                          strong("CONTROL CHART TRENDS"),"displays a control chart for any chosen variable/parameter. Includes lines for upper and lower limits at one and two standard deviations, or you can customize your own limits. Data can be subset by malt style and colored by malt style, raw grain lot number, variety, or crop year.
+                          strong("CONTROL CHART/TRENDS"),"displays a control chart for any chosen variable/parameter. Includes lines for upper and lower limits at one and two standard deviations, or you can customize your own limits. Data can be subset by malt style and colored by malt style, raw grain lot number, variety, or crop year.
 ",
                           br(),
                           br(),
@@ -44,18 +44,24 @@ ui <- navbarPage(title=div(img(src="cropped-CMG_Web_logo_small.png",
 ",
                           br(),
                           br(),
-                          strong("CORRELATION CHART"),"Produces a scatter plot and calculates R-squared value of two chosen variables. Subset data by Style, Crop Year and Variety.
+                          strong("CORRELATION PLOT"),"Produces a scatter plot and calculates R-squared value of two chosen variables. Subset data by Style, Crop Year and Variety.
 ",
+                          br(),
+                          br(),
+                          strong("LOOK UP DATA"),"Creates a table where dataset can be filtered and sorted.",
+                          
                           br(),
                           br(),
                           em('MaltVis was developed by Curtis Davenport and Andrew Caffrey of Admiral Maltings. Feel free to email maltvis@admiralmaltings.com with any questions, comments, or other feedback. Feeling advanced? If you want to run MaltVis locally, or want to contribute new features, find the MaltVis source code on',a(href='https://github.com/curtisdavenport/MaltVis_CraftMaltstersGuild', 'GitHub',target="_blank")),
                           
+                          
+                          
                           style="margin-right:300px; margin-left:20px"),
-                 
-                 
+                        
+                
                  
                  tabPanel("SOP",
-                          tags$iframe(src="Welcome.pdf",height=500,width=850)),
+                          tags$iframe(src="MaltVis SOPs (1).pdf",height=500,width=1200)),
                  
                  
                  tabPanel("Upload",
@@ -64,23 +70,24 @@ ui <- navbarPage(title=div(img(src="cropped-CMG_Web_logo_small.png",
                           ),
                           actionButton("load", "Click Here to Load"),
                           br(),
-                          p('Or use this', a(href='https://docs.google.com/spreadsheets/d/11d3_Wv78W6MCjpSPxNk1lTBJmC8mdDpA3aM-Vz5yewQ/edit#gid=0',"link",target="_blank"), 'to download a sample dataset')),
+                          br(),
+                          p('Or download this', a(href='https://docs.google.com/spreadsheets/d/11d3_Wv78W6MCjpSPxNk1lTBJmC8mdDpA3aM-Vz5yewQ/edit#gid=0',"sample dataset",target="_blank"), 'as a .csv file and then upload it to MaltVis')),
                  
                  
                  
                  tabPanel("BatchVis",
                           textInput("Batch","Batch ID/#"),
-                          sliderInput("visnumber","# of last batches to compare with", min=1,max=33,step=1,value =2),
+                          sliderInput("visnumber","# of last batches to compare with", min=1,max=3,step=1,value = 1),
                           actionButton("batchvisgo","Submit"),
                           p(strong("Style:")),
                           textOutput("Style"),
                           plotlyOutput("batchvis"),
                           dataTableOutput("BatchTable")),
-                 tabPanel("QC Plot",
+                 tabPanel("Control Chart/Trends",
                           selectInput("variable","Variable",choices = NULL),
                           textInput("userupperlimit","Upper Limit"),
                           textInput("userlowerlimit", "Lower Limit"),
-                          sliderInput("graphbatches","# of last batches",min=1,max=3,value = 2),
+                          sliderInput("graphbatches","# of last batches",min=0,max=0,value = 1),
                           pickerInput("style2","Style",choices = NULL,
                                       options = list(`actions-box` = TRUE),
                                       multiple = T),
@@ -89,7 +96,7 @@ ui <- navbarPage(title=div(img(src="cropped-CMG_Web_logo_small.png",
                           plotlyOutput("qcplot"),
                           checkboxInput("dashedline", label = "Dashed Line", value = FALSE),
                           checkboxInput("fitline", label = "Fit Line", value = FALSE)),
-                 tabPanel("Parameter Correlations",
+                 tabPanel("Explore Relationships",
                           selectInput("Corparam", "Parameter", choices = NULL),
                           pickerInput("corstyle","Style",choices=NULL,
                                       options = list(`actions-box` = TRUE),
@@ -102,7 +109,7 @@ ui <- navbarPage(title=div(img(src="cropped-CMG_Web_logo_small.png",
                                       multiple = T),
                           actionButton("CorrGo","Submit"),
                           plotOutput("crosscor")),
-                 tabPanel("Visualize",
+                 tabPanel("Correlation Plot",
                           selectInput("columns1", "Select X Columns", choices = NULL),
                           selectInput("columns2", "Select Y Columns", choices = NULL),
                           pickerInput("style","Style",choices = NULL,
@@ -126,10 +133,8 @@ ui <- navbarPage(title=div(img(src="cropped-CMG_Web_logo_small.png",
                           pickerInput("param","Parameter",choices = NULL,
                                       options = list(`actions-box` = TRUE),
                                       multiple = T),
-                          dataTableOutput("lookupdata")),
-                 
-                 tabPanel("Help",
-                          tags$iframe(src="Help.pdf",height=800,width=850))
+                          dataTableOutput("lookupdata"))
                  
                  
+                
 )
